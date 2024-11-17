08bf24799e4ed11c59b622852c22ef5e66df2394
Yonathan <jhony@Yonathans-MacBook-Pro.local>
2024-11-17 14:55:13 +0200

    Exeption handler

--- src/order_validation.py
+++ src/order_validation.py
@@ -5,16 +5,20 @@ import os
 sqs = boto3.client('sqs')
 
 def lambda_handler(event, context):
-    body = json.loads(event['body'])
-    if 'customer_name' not in body or 'items' not in body:
+    try:
+        body = json.loads(event['body'])
+        if 'customer_name' not in body or 'items' not in body:
+            sqs.send_message(
+                QueueUrl=os.environ['DLQ_URL'],
+                MessageBody=json.dumps({'error': 'Invalid order', 'order': body})
+            )
+            return {'statusCode': 400, 'body': json.dumps({'message': 'Invalid order'})}
+        
         sqs.send_message(
-            QueueUrl=os.environ['DLQ_URL'],
-            MessageBody=json.dumps({'error': 'Invalid order', 'order': body})
+            QueueUrl=os.environ['SQS_QUEUE_URL'],
+            MessageBody=json.dumps(body)
         )
-        return {'statusCode': 400, 'body': 'Invalid order'}
-    
-    sqs.send_message(
-        QueueUrl=os.environ['SQS_QUEUE_URL'],
-        MessageBody=json.dumps(body)
-    )
-    return {'statusCode': 200, 'body': 'Order received'}
+        return {'statusCode': 200, 'body': json.dumps({'message': 'Order received'})}
+    except Exception as e:
+        print(f"Error: {e}")
+        return {'statusCode': 500, 'body': json.dumps({'message': 'Internal server AhAH error'})}
